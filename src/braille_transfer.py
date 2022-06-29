075c6346dd9cc57b7453b38b4d7a09b632463d93
KIM SEOKHYEON <38518675+sh0116@users.noreply.github.com>
2022-06-29 22:36:59 +0900

    Update braille_transfer.py

--- src/braille_transfer.py
+++ src/braille_transfer.py
@@ -1,13 +1,13 @@
 import hbcvt
 
 def main(place="서울 도서관"):
-    rkrk = []
+    braille_list = []
     
     for i in hbcvt.h2b.text(place):
         for j in i[1:]:
             for k in j:
-                rkrk.append(k[1:][0][0])
-    return rkrk
+                braille_list.append(k[1:][0][0])
+    return braille_list
 
 def number_transfer(number=0):
     return  hbcvt.h2b.MATCH_H2B_ALPHABET[str(number)]
